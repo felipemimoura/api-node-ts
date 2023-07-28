df05da44da7c41ae1fea39da3b508c584537dcc4
Felipe Moura <felipemimoura@gmail.com>
2023-07-28 06:18:02 -0300

    feat: add exemple to crud

--- src/index.js
+++ src/index.js
@@ -6,6 +6,17 @@ app.get("/project", function(request, response){
   return response.json(["projeto 01", "projeto 02"])
 })
 
+app.post("/project", function(request, response)  {
+  return response.json(["projeto 01", "projeto 02", "projeto 03"])
+})
+
+app.put("/project/:id", function(request, response)  {
+  return response.json(["projeto 04", "projeto 02", "projeto 03"])
+})
+app.delete("/project/:id", function(request, response)  {
+  return response.json([ "projeto 02", "projeto 03"])
+})
+
 app.listen(3000, function(){
   console.log("SERVER IS RUNNING")
 })
